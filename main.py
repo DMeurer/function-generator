384aef5bfeacda3f5528f9e62a49f6a457240cb1
DMeurer <50107525+DMeurer@users.noreply.github.com>
2021-03-30 20:15:17 +0200

    update main & gitignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from algorithms import linear1
-from algorithms import exponential1
+from algorithms.linear import linear1
+from algorithms.exponential import exponential1
 
+print("starting")
 linear1.alg(2, 1, -5, 4)
 exponential1.alg(5, 1, 6)
