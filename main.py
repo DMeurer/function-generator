29e366f7f41aac32dea0647f963a2d704d25dcc1
DMeurer <50107525+DMeurer@users.noreply.github.com>
2021-03-30 22:04:32 +0200

    fixed linear function bug

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from algorithms.limited_growth import limited_growth
 
 print("starting")
 linear1.alg(2, 1, -5, 4)
+print("----------------------")
 exponential1.alg(5, 1, 6)
+print("----------------------")
 limited_growth.alg(20, 3, 1, 2)
