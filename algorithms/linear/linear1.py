d1b0725d4f293d83afc12e58689f0fd84439b8b7
DMeurer <50107525+DMeurer@users.noreply.github.com>
2021-03-30 20:47:00 +0200

    Update linear1.py

--- algorithms/linear/linear1.py
+++ algorithms/linear/linear1.py
@@ -1,11 +1,10 @@
 def alg(x1, y1, x2, y2):
     print("Method: linear1")
-    print("f(x)=m*(x-d)+c")
+    print("f(x)=m*x+c")
     print("calculating...")
 
     m = (x2 - x1) / (y2 - y1)
-    d = y1
     c = y1 * m + x1
 
     print("done")
-    print("f(x)=" + str(m) + "*(x-(" + str(d) + "))+" + str(c))
+    print("f(x)=" + str(m) + "*x+" + str(c))
