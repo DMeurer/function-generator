3df2896c4fc378097835bf8d072e33a265beb1e9
DMeurer <50107525+DMeurer@users.noreply.github.com>
2021-04-01 12:30:03 +0200

    renaming file to fit system

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from algorithms.linear import linear1
 from algorithms.exponential import exponential1
-from algorithms.limited_growth import limited_growth
+from algorithms.limited_growth import limited_growth1
 
 print("starting")
 linear1.alg(2, 1, -5, 4)
 print("----------------------")
 exponential1.alg(5, 1, 6)
 print("----------------------")
-limited_growth.alg(20, 3, 1, 2)
+limited_growth1.alg(20, 3, 1, 2)
